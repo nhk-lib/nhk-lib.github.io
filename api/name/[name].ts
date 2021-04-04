59a2b3e9586b56bce22c2241a9324efa177d3f15
da99 <sp.03+github.spam@miniuni.com>
2021-04-03 20:32:39 -0500

    Development checkpoint.

--- api/name/[name].ts
+++ api/name/[name].ts
@@ -1,5 +1,6 @@
 
 import { VercelRequest, VercelResponse } from '@vercel/node'
+import fetch from "cross-fetch";
 
 const URLS = {
   channel_101: "http://155.138.139.156:8101/",
@@ -9,12 +10,47 @@ const URLS = {
   jpopsuki: "http://213.239.204.252:8000/"
 }; // object
 
+function get_options(url : URL) {
+  return {
+    method: "GET",
+    hostname: url.hostname,
+    port: parseInt(url.port),
+    path: url.pathname
+  };
+} // function
+
 export default (request: VercelRequest, response: VercelResponse) => {
   const name = request.query.name.toString();
-  response.status(200).json(
-    {
-      name: name,
-      url: URLS[name]
-    }
-  );
-};
+  const url = URLS[name];
+  if (url) {
+    fetch(url)
+    .then( resp => {
+      if (resp.status === 200) {
+        return resp.text();
+      }
+      throw(resp);
+    })
+    .then(function (txt) {
+      response.status(200).json(
+        {
+          name: name,
+          url: URLS[name],
+          body: txt
+        }
+      );
+    })
+    .catch((err) => {
+      response.status(200).json(
+        {
+          name: name,
+          url: URLS[name],
+          error: "UNKNOWN",
+          status: err.status
+        }
+      );
+    });
+    return;
+  }
+
+  response.status(200).json({ error: "INVALID NAME" });
+}; // export
