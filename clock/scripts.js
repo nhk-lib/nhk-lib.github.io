df0fdd90dfe8ce165c9d3c84dec0fbfb06441711
da99 <1551848+da99@users.noreply.github.com>
2022-09-29 17:54:20 -0500

    Added: main clock functionality.

--- clock/scripts.js
+++ clock/scripts.js
@@ -1,2 +1,57 @@
 
+function update_page() {
+  const now        = new Date();
+
+  const hour       = now.getHours();
+  const human_hour = hour % 12;
+  document.getElementById("hour").innerText = human_number(human_hour);
+
+  const minute       = now.getMinutes();
+  document.getElementById("minute").innerText = human_number(minute);
+
+  const seconds = now.getSeconds();
+  document.getElementById("seconds").innerText = human_number(seconds);
+
+  document.getElementById("day").innerText = human_number(now.getDate());
+  document.getElementById("month").innerText = human_number(now.getMonth() + 1);
+  document.getElementById("year").innerText = human_number(now.getYear() - 100);
+  
+  const daynum = now.getDay();
+  switch (daynum) {
+    case 0:
+      document.getElementById("dayname").innerText = 'Sun';
+      break;
+    case 1:
+      document.getElementById("dayname").innerText = 'Mon';
+      break;
+    case 2:
+      document.getElementById("dayname").innerText = 'Tue';
+      break;
+    case 3:
+      document.getElementById("dayname").innerText = 'Wed';
+      break;
+    case 4:
+      document.getElementById("dayname").innerText = 'Thu';
+      break;
+    case 5:
+      document.getElementById("dayname").innerText = 'Fri';
+      break;
+    case 6:
+      document.getElementById("dayname").innerText = 'Sat';
+      break;
+  }
+} // end function
+
 console.log("loaded");
+// update_page();
+
+const x_interval = setInterval(update_page, 1000);
+
+
+function human_number(x) {
+  if (x < 10)
+    return '0' + x.toString();
+  return x.toString();
+}
+
+
